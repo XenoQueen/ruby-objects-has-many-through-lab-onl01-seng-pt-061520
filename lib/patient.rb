5a52410597cc4012cdd898f9db3dbbff43282d5b
Maika Kailani <queenmab051@gmail.com>
2020-07-22 23:41:05 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,26 +2,21 @@ class Patient
 
   attr_accessor :name
 
-  @@all = []
+	def initialize(name)
+		@name = name
+		@appointments = []
+	end
 
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
+	def add_appointment(appointment)
+		@appointments << appointment
+		appointment.patient = self
+	end
 
-  def new_appointment(doctor, date)
-    Appointment.new(date, self, doctor)
-  end
+	def appointments
+		@appointments
+	end
 
-  def self.all
-    @@all
-  end
-
-  def appointments
-    Appointment.all.select { |a| a.patient == self }
-  end
-
-  def doctors
-    appointments.map { |a| a.doctor }
-  end
+	def doctors
+		self.appointments.collect { |appointment| appointment.doctor }
+	end
 end
